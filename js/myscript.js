b0a249c8b7d6072d71d978e60aa756d4ce1fb6cc
Donatella7 <campaniellodonatella@gmail.com>
2022-02-23 15:42:54 +0100

    comment

--- js/myscript.js
+++ js/myscript.js
@@ -36,18 +36,3 @@ userButton.addEventListener("click", function(){
     
     document.getElementById("display-click").classList.remove("d-none");
 });
-
-
-/* CONVALIDA E NON CALCOLA IL PREZZO */
-/* if ( (isNaN(kmUser) || isNaN(ageUser)) || ((kmUser < 0) || (kmUser > 1000)) || ((ageUser < 0) ||(ageUser > 130)) ){
-    document.getElementById("my-ticket-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
-} else if (ageUser < adultAge){
-    calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
-    document.getElementById("my-ticket-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
-} else if(ageUser > elderlyAge){
-    calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
-    document.getElementById("my-ticket-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
-} else{
-    calcoloPrice = (kmUser * kmPrice).toFixed(2);
-    document.getElementById("my-ticket-price").innerHTML = calcoloPrice + (" euro");
-} */
