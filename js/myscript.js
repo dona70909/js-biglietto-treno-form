da7895488613585150b096e70e4f0fbf4a1cb393
Donatella7 <campaniellodonatella@gmail.com>
2022-02-23 15:37:35 +0100

    calcolo prezzo non funziona bene

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,9 @@
 /* User info const */
 let kmUser = document.getElementById("my-km").value;
-let ageUser = document.getElementById("userAge").value;
+let ageUser = document.getElementById("user-age").value;
 let calcoloPrice = 0;
 const kmPrice = 0.21;
-const adultAge = 18;
-const elderlyAge = 65;
 
-
-/* display User info inside the html */
 
 
 
@@ -16,19 +12,24 @@ const elderlyAge = 65;
 const userButton = document.getElementById("my-submit");
 
 userButton.addEventListener("click", function(){
-    /* CONVALIDA E NON CALCOLA IL PREZZO */
+    
     if ( (isNaN(kmUser)) || ((kmUser < 0) || (kmUser > 1000)) ){
         document.getElementById("my-ticket-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
     } else if (ageUser.value == "under"){
         calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
-    } else if(ageUser.value == "65" || ageUser.value == "18"){
+
+    } else if(ageUser.value == "65"){
         calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
-    } else{
+
+    } else {
         calcoloPrice = (kmUser * kmPrice).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = calcoloPrice + (" euro");
     }
+
+    const nameUser = document.getElementById("name-user").value;
+    document.getElementById("print-user-name").innerHTML = nameUser;
     
 });
 
