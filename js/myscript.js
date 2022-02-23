b8f37b3d76b29ba3654b783acc889b177dbb1481
Donatella7 <campaniellodonatella@gmail.com>
2022-02-23 15:26:51 +0100

    ticket price from the input and selection

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 /* User info const */
-let kmUser = parseFloat(prompt("Quanti km vuoi fare?"));
-let ageUser = parseFloat(prompt("Quanti anni hai?"));
+let kmUser = document.getElementById("my-km").value;
+let ageUser = document.getElementById("userAge").value;
 let calcoloPrice = 0;
 const kmPrice = 0.21;
 const adultAge = 18;
@@ -8,53 +8,41 @@ const elderlyAge = 65;
 
 
 /* display User info inside the html */
-document.getElementById("my-km").innerHTML = kmUser + (" km");
-document.getElementById("my-age").innerHTML = ageUser + (" anni");
+
+
+
+
+
+const userButton = document.getElementById("my-submit");
+
+userButton.addEventListener("click", function(){
+    /* CONVALIDA E NON CALCOLA IL PREZZO */
+    if ( (isNaN(kmUser)) || ((kmUser < 0) || (kmUser > 1000)) ){
+        document.getElementById("my-ticket-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
+    } else if (ageUser.value == "under"){
+        calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
+        document.getElementById("my-ticket-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
+    } else if(ageUser.value == "65" || ageUser.value == "18"){
+        calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
+        document.getElementById("my-ticket-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
+    } else{
+        calcoloPrice = (kmUser * kmPrice).toFixed(2);
+        document.getElementById("my-ticket-price").innerHTML = calcoloPrice + (" euro");
+    }
+    
+});
 
 
 /* CONVALIDA E NON CALCOLA IL PREZZO */
-if ( (isNaN(kmUser) || isNaN(ageUser)) || ((kmUser < 0) || (kmUser > 1000)) || ((ageUser < 0) ||(ageUser > 130)) ){
-    document.getElementById("my-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
+/* if ( (isNaN(kmUser) || isNaN(ageUser)) || ((kmUser < 0) || (kmUser > 1000)) || ((ageUser < 0) ||(ageUser > 130)) ){
+    document.getElementById("my-ticket-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
 } else if (ageUser < adultAge){
     calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
-    document.getElementById("my-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
+    document.getElementById("my-ticket-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
 } else if(ageUser > elderlyAge){
     calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
-    document.getElementById("my-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
+    document.getElementById("my-ticket-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
 } else{
     calcoloPrice = (kmUser * kmPrice).toFixed(2);
-    document.getElementById("my-price").innerHTML = calcoloPrice + (" euro");
-}
-
-
-
-
-/* CONVALIDA E RANDOM VALUE   */
-
-/* convalida dei dati : dati creati random se non sono giusti */
-/* !convalida km */
-/* if(isNaN(kmUser)){
-    kmUser = Math.floor(Math.random() * 100) + 1; 
-    document.getElementById("my-km").innerHTML = ("km inseriti dal pc ") +  kmUser + (" km");
-} 
-
-/* !convalida age */
-/* if(isNaN(ageUser)){
-    ageUser = Math.floor(Math.random() * 10) + 1; 
-    document.getElementById("my-age").innerHTML = ageUser +(" anni");
-} */ 
-
-
-/* if (ageUser <= adultAge){
-    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100)).toFixed(2);
-    document.getElementById("my-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
-} else if(ageUser > elderlyAge){
-    calcoloPrice = ((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100)).toFixed(2);
-    document.getElementById("my-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
-} else{
-    calcoloPrice = (kmUser * kmPrice).toFixed(2);
-    document.getElementById("my-price").innerHTML = calcoloPrice + (" euro");
+    document.getElementById("my-ticket-price").innerHTML = calcoloPrice + (" euro");
 } */
-
-
-
