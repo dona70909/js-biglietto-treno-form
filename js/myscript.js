bbe2533b7e74d1c7ca99c19fc6ab2bdb2b3141b4
Donatella7 <campaniellodonatella@gmail.com>
2022-02-23 17:49:02 +0100

    problema input

--- js/myscript.js
+++ js/myscript.js
@@ -1,35 +1,37 @@
 
 
-let calcoloPrice = 0;
-const kmPrice = 0.21;
-
 /* event listener calcolo */
 const userButton = document.getElementById("my-submit");
 
 userButton.addEventListener("click", function(){
+    
     /* User info */
-let kmUser = parseFloat(document.getElementById("my-km").value);
-let ageUser = document.getElementById("user-age").value;
+    let kmUser = parseFloat(document.getElementById("my-km").value);
+    let ageUser = document.getElementById("user-age").value;
+    let calcoloPrice = 0;
+    const kmPrice = 0.21;
+
     if ( (isNaN(kmUser)) || ((kmUser < 0) || (kmUser > 1000)) ){
         document.getElementById("my-ticket-price").innerHTML = ("I Dati inserti non sono corretti. Non posso calcolare il prezzo.");
     } else if (ageUser == "under"){
         calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (20)) / 100))).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = ("UNDER 18 PRICE ") + calcoloPrice + (" euro");
-
+        
     } else if(ageUser == "65"){
         calcoloPrice = (((kmUser * kmPrice) - (((kmUser * kmPrice) * (40)) / 100))).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = ("OVER 65 PRICE ") + calcoloPrice + (" euro");
-
+        
     } else if(ageUser == "18") {
         calcoloPrice = (kmUser * kmPrice).toFixed(2);
         document.getElementById("my-ticket-price").innerHTML = calcoloPrice + (" euro");
     }
-
-    const nameUser = document.getElementById("name-user").value;
+    
+    let nameUser = document.getElementById("name-user");
     document.getElementById("print-user-name").innerHTML = nameUser;
-
-    const surnameUser = document.getElementById("surname-user").value;
-    document.getElementById("print-user-surname").innerHTML = nameUser;
+    
+    
+    let surnameUser = document.getElementById("surname-user");
+    document.getElementById("print-user-surname").innerHTML = surnameUser;
     
     document.getElementById("display-click").classList.remove("d-none");
 });
